70169393cd50c6985dfe0d10ac92693b721151c8
wkibbler <william.kibbler@googlemail.com>
2020-09-14 21:49:20 +0100

    transaction encoding

--- test/transaction.js
+++ test/transaction.js
@@ -1,28 +1,23 @@
 var sia = require('../lib');
 
-var inputs = [
+var inputs_from_api = [
     {
-        parentID: 'b8c63a8f435bfff7bf8c1f6c7ece0066599fa4e08cb74ab5929e84b014e408c8',
-        unlockConditions: {
-            publicKeys: [ 'ed25519:8408ad8d5e7f605995bdf9ab13e5c0d84fbe1fc610c141e0578c7d26d5cfee75' ],
-            signaturesRequired: 1
-        }
+        "output": "c67c0f52c9ae5a3003a62bcb86354bedfb7037065143a590489c654dde2ce091",
+        "hastings": "2961935000000000000000000000",
+        "sf": null
     }
 ]
 
-var outputs = [
-    // destination
-    {
-        value: 100000,
-        unlockHash: '5ac6af95fe284b4bbb0110ef51d3c90f3e9ea37586352ec83bad569230bad7f37a452c0a2a2f'
-    },
-    // change (users address)
-    {
-        value: 100000,
-        unlockHash: 'df1b42c80b5f7a67331893fde0923a5071d6d7dff4c78baec547cf5ca4d314a1d78b6b1c8d42'
-    }
-]
+var fee = 0.1;
 
-var fee = 13000;
+var senders_public_key = '8408ad8d5e7f605995bdf9ab13e5c0d84fbe1fc610c141e0578c7d26d5cfee75';
 
-var transaction = sia.transction.encodeTransaction(inputs, outputs, fee);
+var senders_address = '5ac6af95fe284b4bbb0110ef51d3c90f3e9ea37586352ec83bad569230bad7f37a452c0a2a2f';
+
+var send_amount = 10;
+
+var receivers_address = 'df1b42c80b5f7a67331893fde0923a5071d6d7dff4c78baec547cf5ca4d314a1d78b6b1c8d42';
+
+var transaction = sia.transction.encodeTransaction(inputs_from_api, fee, senders_public_key, senders_address, send_amount, receivers_address, (res) => {
+    console.log(res)
+});
