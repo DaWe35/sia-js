87ff40f5a9d498eb35bd112c8cc96c0cbef84c6e
wkibbler <william.kibbler@googlemail.com>
2020-09-15 18:27:09 +0100

    transaction fix and priv to pub key function

--- test/transaction.js
+++ test/transaction.js
@@ -12,7 +12,7 @@ var inputs_from_api = [
 
 var fee = 0.1;
 
-var senders_public_key = 'a850a034bd12eb0523ea8b2befadde67494b36db69120b1bf111247c2e81f3a2';
+var senders_public_key = '2002dfde51fdf9fae9ec62f8fff5f7cb1af430f92626691e73ecbf343a4a8a6b';
 
 var senders_address = '3a0118104939d2f550e7e3b50840473f53494648eaaf01f8b89bba57ae9f12f41e4c67e2ab9b';
 
@@ -23,6 +23,7 @@ var send_amount = 10;
 var receivers_address = 'df1b42c80b5f7a67331893fde0923a5071d6d7dff4c78baec547cf5ca4d314a1d78b6b1c8d42';
 
 var transaction = sia.transction.encodeTransaction(inputs_from_api, fee, senders_public_key, senders_address, send_amount, receivers_address, senders_private_key, (res) => {
+    console.log(res)
     fetch('http://escher-walrus.libtechnologies.io/broadcast', {
         method: 'POST',
         body: JSON.stringify([res])
