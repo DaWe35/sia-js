733e79895f082e4c7ed31834c29aee58ec32b09d
wkibbler <william.kibbler@googlemail.com>
2020-09-15 19:53:14 +0100

    change bitint lib

--- test/transaction.js
+++ test/transaction.js
@@ -4,15 +4,13 @@ var util = require('util');
 
 var inputs_from_api = [
     {
-        "output": "c67c0f52c9ae5a3003a62bcb86354bedfb7037065143a590489c654dde2ce091",
-        "hastings": "2961935000000000000000000000",
+        "output": "e4974950a58def66bb5e7b51b5208ec665bce07684e006d9e4b451c4956790e7",
+        "hastings": "2941735000000000000000000000",
         "sf": null
     }
 ]
 
 var fee = 0.1;
-
-var senders_public_key = '2002dfde51fdf9fae9ec62f8fff5f7cb1af430f92626691e73ecbf343a4a8a6b';
 
 var senders_address = '3a0118104939d2f550e7e3b50840473f53494648eaaf01f8b89bba57ae9f12f41e4c67e2ab9b';
 
@@ -22,10 +20,10 @@ var send_amount = 10;
 
 var receivers_address = 'df1b42c80b5f7a67331893fde0923a5071d6d7dff4c78baec547cf5ca4d314a1d78b6b1c8d42';
 
-var transaction = sia.transction.encodeTransaction(inputs_from_api, fee, senders_public_key, senders_address, send_amount, receivers_address, senders_private_key, (res) => {
+var transaction = sia.transction.encodeTransaction(inputs_from_api, fee, senders_address, send_amount, receivers_address, senders_private_key, (res) => {
     console.log(res)
     fetch('http://escher-walrus.libtechnologies.io/broadcast', {
         method: 'POST',
         body: JSON.stringify([res])
-    }).then(remoteRes => remoteRes.text()).then(json => console.log(json))
+    }).then(remoteRes => remoteRes.jdon()).then(json => console.log(json))
 });
